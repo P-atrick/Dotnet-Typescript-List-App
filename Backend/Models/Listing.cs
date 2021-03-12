212fb06cc040d6cefbdeca6390fa96f8c8f39f8a
Patrick Kelly <patrick@pkelly.co>
2021-03-12 21:17:16 +0000

    Add listing class

---
+++ Backend/Models/Listing.cs
@@ -0,0 +1,10 @@
+namespace Backend.Models
+{
+  public class Listing
+  {
+    public string Company { get;set; }
+    public string Title { get;set; }
+    public string Url { get;set; }
+    public string Notes { get;set; }
+  }
+}
