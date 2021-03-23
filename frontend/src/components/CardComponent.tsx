88b1f7c4355ecb9aa951725cc493baf0c7d87d21
Patrick Kelly <patrick@pkelly.co>
2021-03-23 21:12:00 +0000

    Add delete listing button

--- frontend/src/components/CardComponent.tsx
+++ frontend/src/components/CardComponent.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import Axios from 'axios';
 import Card from '@material-ui/core/Card';
 import { Listing } from './Home';
 
@@ -6,11 +7,19 @@ type CardComponentProps = {
   listing: Listing
 }
 
+const handleDelete = (listingId: string) => {
+  Axios
+    .delete(`http://localhost:5000/api/listings/${listingId}`)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
 const CardComponent: FunctionComponent<CardComponentProps> = ({listing}) => {
   console.log(listing)
   return (
     <Card key={listing.id}>
       {listing.company}
+      <button onClick={() => handleDelete(listing.id)}>Del</button>
     </Card>
   )
 }
